093540e66854f69099a1e05ea378244a377d994c
max-romagnoli <massimiliano.romagnoli02@gmail.com>
2025-03-30 12:52:49 +0100

    Dis

--- field_manager/views/project_join_view.py
+++ field_manager/views/project_join_view.py
@@ -74,7 +74,7 @@ class ProjectJoinView(APIView):
         )
 
         # template_dir = "field_manager/"      # TODO: Can have many more templates
-        template_dir = "field_manager/qgis_templates/portrane"
+        template_dir = "field_manager/qgis_templates/hist_qcc"
         instance_dir = create_project_folder_for_instance(instance, template_dir)
 
         # Return instance info
