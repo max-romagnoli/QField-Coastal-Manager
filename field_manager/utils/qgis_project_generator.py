4dbea95a7723ebd857dd8f5f7a974310b43f0815
max-romagnoli <massimiliano.romagnoli02@gmail.com>
2025-03-30 13:12:36 +0100

    qgz

--- field_manager/utils/qgis_project_generator.py
+++ field_manager/utils/qgis_project_generator.py
@@ -25,7 +25,7 @@ def create_project_folder_for_instance(instance, template_dir):
     qgs_filename = _find_qgis_project_file(instance_dir)
     if qgs_filename:
         old_qgs_filename = os.path.join(instance_dir, qgs_filename)
-        new_qgs_filename = os.path.join(instance_dir, f"{instance.project.name}.qgs")
+        new_qgs_filename = os.path.join(instance_dir, f"{instance.project.name}.qgz") # FIXME:
         os.rename(old_qgs_filename, new_qgs_filename)
 
     relative_path = os.path.relpath(instance_dir, settings.MEDIA_ROOT)
